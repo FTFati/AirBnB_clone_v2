bd1f4c1b6f84a13ed714175254f68b3da8e3f5d3
ZZZHajar008 <ilyass.infinite345@gmail.com>
2024-01-16 22:40:00 +0100

    TESTS

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,49 +1,29 @@
 #!/usr/bin/python3
-"""Unittest test_review module"""
-
-import unittest
-import pycodestyle
-from models.engine.file_storage import FileStorage
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
-class TestReview(unittest.TestCase):
-    """Test cases for TestReview class"""
+class test_review(test_basemodel):
+    """ """
 
-    def setUp(self):
-        """Set up test cases"""
-        self.storage = FileStorage()
-        self.storage.reload()
-        self.storage._FileStorage__objects = {}
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Review"
+        self.value = Review
 
-    def tearDown(self):
-        """Clean up after test cases"""
-        self.storage._FileStorage__objects = {}
+    def test_place_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.place_id), str)
 
-    def test_review(self):
-        """Test the Review class"""
-        my_review = Review()
-        my_review.place_id = "0001"
-        my_review.review_id = "0002"
-        my_review.text = "Great place, had a wonderful time!"
-        my_review.save()
+    def test_user_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.user_id), str)
 
-        all_objs = self.storage.all()
-        key = f'Review.{my_review.id}'
-        self.assertIn(key, all_objs.keys())
-
-    def test_pycodestyle(self):
-        """Test that the code follows pycodestyle guidelines"""
-        style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['models/review.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_module_docstring(self):
-        """Test that the module has a docstring"""
-        import models.review
-        self.assertIsNotNone(models.review.__doc__)
-
-    def test_class_docstring(self):
-        """Test that the class has a docstring"""
-        self.assertIsNotNone(Review.__doc__)
+    def test_text(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.text), str)
