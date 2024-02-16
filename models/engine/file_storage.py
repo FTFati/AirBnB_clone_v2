5cc916d34e6ecba7b5b09ef5fef34d61bced4f01
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-02-16 18:27:18 +0100

    Fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,5 +68,5 @@ class FileStorage:
                 self.save()
 
     def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
+        """Call the reload method."""
         self.reload()
